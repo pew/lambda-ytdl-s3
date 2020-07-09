9268a25171f766db5bc63999f966703f49176b00
Jonas Wagner <boom@confuflux.com>
2020-07-09 17:18:34 +0200

    I'm not sure why I did that

--- README.md
+++ README.md
@@ -6,39 +6,39 @@ It's not really a good idea to download files using lambda since there's only a 
 
 Maybe there's a way to stream the files directly to S3, there probably is! But not in this project. Yet!
 
-# install dependencies
+## install dependencies
 
 in a virtualenv, please.
 
-```
+```shell
 pip install -r requirements.txt
 ```
 
-# package everything for lambda
+## package everything for lambda
 
 lambda requires everything pre-packaged so it can be invoked quickly. boto3 is already provided by AWS.
 
 example path on my mac, you'll see the path while executing `pip install`
 
-```
+```shell
 cd ~/.pyenv/versions/3.6.3/envs/lambdas3/lib/python3.6/site-packages
 zip -r9 ~/lambda-ytdl-s3/lambda-ytdl-s3.zip
 ```
 
 now, add `app.py` to the zip as well (go into this folder again)
 
-```
+```shell
 zip -g lambda-ytdl-s3.zip app.py
 ```
 
 using tools like `zappa` or `serverless` will make this process way easier I guess.
 
-# next steps
+## next steps
 
 * upload the generated zip to s3
 * create new lambda function
 * use the s3 link for your `function code` (download from s3 URL)
 * set the lambda `handler` to `app.lambda_handler`
-* set an environment variable in lambda `s3_bucket` to your bucket name you want the videos to end up
+* set an environment variable in lambda `S3_BUCKET` to your bucket name you want the videos to end up
 * create an IAM role/policy so that the function is allowed to write into the S3 bucket
 * create a `test` function in the lambda console and point it to a cool video to download it!
--- app.py
+++ app.py
@@ -5,44 +5,55 @@ import youtube_dl
 import os
 
 # define s3 bucket
-s3_bucket = os.environ.get('s3_bucket')
+s3_bucket = os.environ["S3_BUCKET"]
+region = os.environ["AWS_REGION"]
 
 # define boto client
-s3 = boto3.client('s3', region_name='us-east-1')
+s3 = boto3.client("s3", region_name=region)
 
 # define youtube-dl options
 ytdl_options = {
-    # 'format': 'bestvideo[ext=mp4]+bestaudio[ext=m4a]/mp4',
-    'outtmpl': '/tmp/%(title)s.%(ext)s',
-    'restrictfilenames': True,
-    'cachedir': False
+    "format": "bestvideo[ext=mp4]+bestaudio[ext=m4a]/best[ext=mp4]/best",
+    "outtmpl": "/tmp/%(title)s.%(ext)s",
+    "restrictfilenames": True,
+    "cachedir": False,
 }
 
 
 def download_file(video):
-    ytdl = youtube_dl.YoutubeDL(ytdl_options)
+    try:
+        ytdl = youtube_dl.YoutubeDL(ytdl_options)
+    except:
+        raise
     info = ytdl.extract_info(video, download=True)
     return ytdl.prepare_filename(info)
 
 
 def s3_upload(filename):
     """upload file to s3"""
-    print('uploading {} to s3'.format(filename))
-    s3.upload_file(filename, s3_bucket, os.path.basename(filename))
-
-    return True  # fix at some point
+    try:
+        print("uploading {} to s3".format(filename))
+        s3.upload_file(filename, s3_bucket, os.path.basename(filename))
+    except:
+        raise
+    return True
 
 
 def lambda_handler(event, context):
     """aws lambda handler to down/upload files."""
     input_url = event
-    filename = download_file(input_url)
 
-    download_file(input_url)
-
-    if download_file:
+    try:
+        filename = download_file(input_url)
+    except:
+        raise
+    try:
         upload = s3_upload(filename)  # trigger s3 upload
-        if upload:
-            return True
+    except:
+        raise
 
-# invoke like this: "https://www.youtube.com/watch?v=oHg5SJYRHA0"
+    return True
+
+
+if __name__ == "__main__":
+    lambda_handler("https://www.youtube.com/watch?v=7bXjWRXDFV8", None)
